97ecab35b67ffc71f4b1bf722ad9007e340c366b
philiphan0109 <philiphan0109@hotmail.com>
2024-06-19 18:13:33 -0700

    Update comment_extraction.py

--- comment_extraction.py
+++ comment_extraction.py
@@ -1,8 +1,35 @@
 import os
 import re
-from tools import extract_title, extract_abstract, extract_comments, extract_authors
 
 dir_path = "paper/"
+
+def extract_comments(path):
+    comments = {}
+
+    with open(path, "r", encoding="utf-8") as file:
+        full_paper = file.read()
+    
+    current_index = 0
+    comment_start_index = -1
+    current_comment = []
+    
+    lines = full_paper.split("\n")
+    for line in lines:
+        stripped_line = line
+        
+        if line.startswith('%'):
+            if not current_comment:
+                comment_start_index = current_index
+            current_comment.append(line.strip())
+        else: 
+            if current_comment:
+                comments[comment_start_index] = current_comment
+                current_comment = []
+                
+                
+        current_index += len(line) + 1
+        
+    return comments
 
 all_comments = {}
 for filename in os.listdir(dir_path):
@@ -12,6 +39,6 @@ for filename in os.listdir(dir_path):
 
 for file, comments in all_comments.items():
     print(f"Comments from {file}")
-    for comment in comments:
-        print(f"- {comment}")
+    for char, comment in comments.items():
+        print(f"{char}: {comment}")
     print("\n")
