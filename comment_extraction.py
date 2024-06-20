37637cff6b21a464b04d7bb98bb8171102de77aa
philiphan0109 <philiphan0109@hotmail.com>
2024-06-20 11:04:51 -0700

    update

--- comment_extraction.py
+++ comment_extraction.py
@@ -11,42 +11,6 @@ nltk.download('punkt')
 
 dir_path = "paper/"
 
-def extract_comment_text(path):
-    comments = {}
-
-    with open(path, "r", encoding="utf-8") as file:
-        full_paper = file.read()
-    
-    current_index = 0
-    comment_start_index = -1
-    current_comment = []
-    
-    lines = full_paper.split("\n")
-    for line in lines:
-        if line.startswith('%'): # This means that the line is a full line comment
-            if not current_comment:
-                comment_start_index = current_index
-            current_comment.append(line.strip())
-        else: # If it's not a full line comment, it can mean two things
-            
-            # 1) there is a comment at the END of the line (this excludes any \%s)
-            end_of_line_comment = re.search(r'(?<!\\)%.*$', line)
-            if end_of_line_comment:
-                comment_start_index = current_index + end_of_line_comment.start()
-                comments[comment_start_index] = [end_of_line_comment.group()]
-            
-            if current_comment: # 2) this line is not a part of a block comment anymore, so save the previous block comment to the dictionary
-                comments[comment_start_index] = current_comment
-                current_comment = []
-                
-        current_index += len(line) + 1
-    # one last save
-    if current_comment: 
-        comments[comment_start_index] = current_comment
-        current_comment = []
-    
-    return comments
-
 def extract_comment_indices(path):
     comment_indices = []
 
@@ -110,41 +74,44 @@ def extract_comment_statistics(path):
     
     return comment_statistics
 
-# all_comment_indices = {}
-# for filename in os.listdir(dir_path):
-#     if filename.endswith(".tex"):
-#         file_path = os.path.join(dir_path, filename)
-#         all_comment_indices[filename] = extract_comment_indices(file_path)
+def debug(path, context=5):
+    comment_indices = extract_comment_indices(path)
+    
+    with open(path, "r", encoding="utf-8") as file:
+        full_paper = file.read()
+    
+    comments_with_stats = {}
+    
+    for start, end in comment_indices:
+        start_context = max(0, start - context)
+        end_context = min(len(full_paper), end + context)
+        comment_text_with_context = full_paper[start_context:end_context]
+        comment_text = full_paper[start:end]
+        
+        char_length = len(comment_text)
+        words = word_tokenize(comment_text)
+        words = [word for word in words if word.isalnum()]
+        word_count = len(words)
+        
+        comments_with_stats[(start, end)] = {
+            'text_with_context': comment_text_with_context,
+            'char_length': char_length,
+            'word_count': word_count
+        }
+    
+    return comments_with_stats
 
-# for file, comment_indices in all_comment_indices.items():
-#     print(f"Comments from {file}")
-#     for comment in comment_indices:
-#         print(f"start: {comment[0]} | stop: {comment[1]}")
-#     print("\n")
-
-def main():
-    comment_statistics = {}
-    for filename in os.listdir(dir_path):
-        if filename.endswith(".tex"):
-            file_path = os.path.join(dir_path, filename)
-            comment_statistics[filename] = extract_comment_statistics(file_path)
-
-    for file, statistics in comment_statistics.items():
-        print(f"Statistics from {file}")
-        for char, stats in statistics.items():
-            print(f"{char}: {stats}")
+all_comments_with_stats = {}
+for filename in os.listdir(dir_path):
+    if filename.endswith(".tex"):
+        file_path = os.path.join(dir_path, filename)
+        all_comments_with_stats[filename] = debug(file_path)
+    
+for file, comments in all_comments_with_stats.items():
+    print(f"Comments and statistics from {file}")
+    for (start, end), stats in comments.items():
+        print(f"Start: {start}, End: {end}")
+        print(f"Text with context: {stats['text_with_context']}")
+        print(f"Character length: {stats['char_length']}")
+        print(f"Word count: {stats['word_count']}")
         print("\n")
-
-
-if __name__ == "__main__":
-    pr = cProfile.Profile()
-    pr.enable()
-
-    main()
-
-    pr.disable()
-    s = io.StringIO()
-    sortby = 'cumulative'
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
