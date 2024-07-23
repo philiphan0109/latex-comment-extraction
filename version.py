d527b025f028142ebf0ff541da8c86e2f1f713d2
philiphan0109 <philiphan0109@hotmail.com>
2024-07-23 10:30:55 -0700

    Update version.py

--- version.py
+++ version.py
@@ -1,26 +1,29 @@
 import os
 import json
+from collections import defaultdict
 
 def get_versions(dir_path: str) -> dict:
-    versions = {}
+    versions = defaultdict(list)
     for paper in os.listdir(dir_path):
         paper_path = os.path.join(dir_path, paper)
         metadata_path = os.path.join(paper_path, "metadata.json")
         with open(metadata_path, "r") as f:
             metadata = json.load(f)
             url = metadata["url"]
-            if url[-2] != 'v':
-                print("no version")
-            elif (url[-2:] not in versions.keys()):
-                versions[url[-2:]] = 1
-            else: 
-                versions[url[-2:]] += 1
+            arxiv_id = metadata["id"]
+            
+            if url[-2] == 'v':
+                version = url[-2:]
+                versions[version].append(arxiv_id)
+            else:
+                print(f"No version found for paper with ID: {arxiv_id}")
+    
     return versions
-
 
 if __name__ == "__main__":
     path = "test_set/"
     versions = get_versions(path)
-    for version in versions.items():
-        print(version)
-    
+    for version, ids in versions.items():
+        print(f"Version {version}: {len(ids)} papers")
+        print(f"ArXiv IDs: {', '.join(ids)}")
+        print()
